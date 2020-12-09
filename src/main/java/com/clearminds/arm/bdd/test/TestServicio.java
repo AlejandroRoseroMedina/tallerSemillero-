5d622c54cf890f30566e957414b4fec9cbbda308
Alejandro Rosero <ralejandro@unicauca.edu.co>
2020-12-09 14:43:48 -0500

    Commit déspues de relacionarse con con el proyecto Model

---
+++ src/main/java/com/clearminds/arm/bdd/test/TestServicio.java
@@ -0,0 +1,27 @@
+package com.clearminds.arm.bdd.test;
+
+import java.sql.SQLException;
+
+import com.clearminds.arm.dtos.Estudiante;
+import com.clearminds.arm.excepciones.BDDException;
+import com.clearminds.arm.servicios.ServicioEstudiante;
+
+public class TestServicio {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ServicioEstudiante srvEstudiante=new ServicioEstudiante();
+		try {
+			srvEstudiante.insertarEstudiante(new Estudiante("Juan", "Perez"));
+		} catch (BDDException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			e.getMessage();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+}
--- src/main/java/com/clearminds/arm/servicios/ServicioEstudiante.java
+++ src/main/java/com/clearminds/arm/servicios/ServicioEstudiante.java
@@ -1,5 +1,18 @@
 package com.clearminds.arm.servicios;
 
+import java.sql.SQLException;
+
+import com.clearminds.arm.dtos.Estudiante;
+import com.clearminds.arm.excepciones.BDDException;
+
 public class ServicioEstudiante extends ServicioBase{
+	
+	public void insertarEstudiante(Estudiante estu) throws BDDException, SQLException{
+		ServicioEstudiante srvEstudiante = new ServicioEstudiante();
+		srvEstudiante.abrirConexion();
+		System.out.println("Insertando estudiante: "+estu);
+		srvEstudiante.cerrarConexion();
+	
+	}
 
 }
